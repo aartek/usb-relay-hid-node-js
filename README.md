ee24964aeefa4abe19f83876e81205c2ae37d926
Artur Nowakowski <artur.nowakowski@sap.com>
2017-10-02 19:17:47 +0200

    added api documentation

--- README.md
+++ README.md
@@ -10,3 +10,300 @@ It is based on the following project:
 
 API
 -----------
+
+#### api.init() 
+
+Initialize the USB Relay Library
+
+
+
+
+
+
+##### Returns
+
+
+- `number`  returns 0 on success and -1 on error.
+
+
+
+#### api.exit() 
+
+Finalize the USB Relay Library.
+This function frees all of the static data associated with USB Relay Library.
+It should be called at the end of execution to avoid memory leaks.
+
+
+
+
+
+
+##### Returns
+
+
+- `number`  returns 0 on success and -1 on error.
+
+
+
+#### api.libVersion() 
+
+Get the library (dll) version
+
+
+
+
+
+
+##### Returns
+
+
+- `TODO`  Lower 16 bits: the library version. Higher bits: undefined, ignore.
+
+
+
+#### api.listDevices() 
+
+Get the list of available devices
+
+
+
+
+
+
+##### Returns
+
+
+- `deviceInfo`  List of devices containing devicePtr - pointer to the device, serialNumber - device serial number, type - number of relays
+
+
+
+#### api.getDeviceId(devicePtr) 
+
+Get device id
+
+
+
+
+##### Parameters
+
+| Name | Type | Description |  |
+| ---- | ---- | ----------- | -------- |
+| devicePtr | `devicePtr`  | pointer to the device | &nbsp; |
+
+
+
+
+##### Returns
+
+
+- `number`  the ID string of the device.
+
+
+
+#### api.getDeviceRelaysNumber(devicePtr) 
+
+Get number of relay channels on the device
+
+
+
+
+##### Parameters
+
+| Name | Type | Description |  |
+| ---- | ---- | ----------- | -------- |
+| devicePtr | `devicePtr`  | pointer to the device | &nbsp; |
+
+
+
+
+##### Returns
+
+
+- `number`  number of available relay channels
+
+
+
+#### api.openDeviceWithSerial(serialNumber) 
+
+Open device that serial number is serialNumber
+
+
+
+
+##### Parameters
+
+| Name | Type | Description |  |
+| ---- | ---- | ----------- | -------- |
+| serialNumber | `string`  | device serial number | &nbsp; |
+
+
+
+
+##### Returns
+
+
+- `deviceHandler`  returns a valid handle to the device on success or NULL on failure
+
+
+
+#### api.openDevice(devicePtr) 
+
+Open a USB relay device
+
+
+
+
+##### Parameters
+
+| Name | Type | Description |  |
+| ---- | ---- | ----------- | -------- |
+| devicePtr | `devicePtr`  | pointer to the device | &nbsp; |
+
+
+
+
+##### Returns
+
+
+- `deviceHandler`  returns a valid handle to the device on success or NULL on failure.
+
+
+
+#### api.closeDevice(deviceHandler) 
+
+Close a USB relay device
+
+
+
+
+##### Parameters
+
+| Name | Type | Description |  |
+| ---- | ---- | ----------- | -------- |
+| deviceHandler | `deviceHandler`  | device handler which usb relay device your want to operate | &nbsp; |
+
+
+
+
+##### Returns
+
+
+-  TODO
+
+
+
+#### api.openChannel(deviceHandler, channelNumber) 
+
+Turn ON a relay channel on the USB-Relay-Device
+
+
+
+
+##### Parameters
+
+| Name | Type | Description |  |
+| ---- | ---- | ----------- | -------- |
+| deviceHandler | `deviceHandler`  | device handler which usb relay device your want to operate | &nbsp; |
+| channelNumber | `channelNumber`  | the channel number: 1...max | &nbsp; |
+
+
+
+
+##### Returns
+
+
+- `number`  0 -- success; 1 -- error; 2 -- index is invalid
+
+
+
+#### api.closeChannel(deviceHandler, channelNumber) 
+
+Turn OFF a relay channel on the USB-Relay-Device
+
+
+
+
+##### Parameters
+
+| Name | Type | Description |  |
+| ---- | ---- | ----------- | -------- |
+| deviceHandler | `deviceHandler`  | device handler which usb relay device your want to operate | &nbsp; |
+| channelNumber | `number`  | the channel number: 1...max | &nbsp; |
+
+
+
+
+##### Returns
+
+
+- `number`  0 -- success; 1 -- error
+
+
+
+#### api.openAllChannels(deviceHandler) 
+
+Turn ON all relay channels on the USB-Relay-Device
+
+
+
+
+##### Parameters
+
+| Name | Type | Description |  |
+| ---- | ---- | ----------- | -------- |
+| deviceHandler | `deviceHandler`  | device handler which usb relay device your want to operate | &nbsp; |
+
+
+
+
+##### Returns
+
+
+- `number`  0 -- success; 1 -- error
+
+
+
+#### api.closeAllChannels(deviceHandler) 
+
+Turn OFF all relay channels on the USB-Relay-Device
+
+
+
+
+##### Parameters
+
+| Name | Type | Description |  |
+| ---- | ---- | ----------- | -------- |
+| deviceHandler | `deviceHandler`  | device handler which usb relay device your want to operate | &nbsp; |
+
+
+
+
+##### Returns
+
+
+- `number`  0 -- success; 1 -- error
+
+
+
+#### api.getStatus() 
+
+Get state of all channels of the  USB-Relay-Device
+
+
+
+
+##### Parameters
+
+| Name | Type | Description |  |
+| ---- | ---- | ----------- | -------- |
+|  | `deviceHandler`  |  | &nbsp; |
+
+
+
+
+##### Returns
+
+
+-  TODO
+
--- USBRelay.js
+++ USBRelay.js
@@ -28,30 +28,53 @@ var USBRelay = (function(api){
 				type: api.getDeviceRelaysNumber(devicePtr)
 			}
 	}
-	
+
+    /**
+	 * Initialize the USB Relay Library
+     * @returns {number} returns 0 on success and -1 on error.
+     */
 	api.init = function(){
 		return usbLib.usb_relay_init();
 	}
-	
+
+    /**
+	 * Finalize the USB Relay Library.
+     * This function frees all of the static data associated with USB Relay Library.
+     * It should be called at the end of execution to avoid memory leaks.
+     * @returns {number} returns 0 on success and -1 on error.
+     */
 	api.exit = function(){
 		return usbLib.usb_relay_exit;
 	}
-	
+
+    /**
+	 * Get the library (dll) version
+     * @returns {TODO} Lower 16 bits: the library version. Higher bits: undefined, ignore.
+     */
 	api.libVersion = function(){
 		return usbLib.usb_relay_device_lib_version;
 	}
-	
+
+    /**
+	 * Get the list of available devices
+     * @returns {deviceInfo} List of devices containing devicePtr - pointer to the device, serialNumber - device serial number, type - number of relays
+     */
 	api.listDevices = function(){
 		let list = [];
 		let head = usbLib.usb_relay_device_enumerate();
-		let elemPtr = head;		
+		let elemPtr = head;
 		while(!elemPtr.isNull()){
 			list.push(buildDeviceObject(elemPtr));
 			elemPtr = usbLib.usb_relay_device_next_dev(elemPtr)
-		}		
+		}
 		return list;
 	};
-	
+
+    /**
+	 * Get device id
+     * @param {devicePtr} devicePtr pointer to the device
+     * @returns {number} the ID string of the device.
+     */
 	api.getDeviceId = function(devicePtr){
 		let idPtr =  usbLib.usb_relay_device_get_id_string(devicePtr)
 		if(!idPtr.isNull()){
@@ -59,39 +82,86 @@ var USBRelay = (function(api){
 		}
 		return null;
 	}
-	
+
+    /**
+	 * Get number of relay channels on the device
+     * @param {devicePtr} devicePtr pointer to the device
+     * @returns {number} number of available relay channels
+     */
 	api.getDeviceRelaysNumber = function(devicePtr){
 		return usbLib.usb_relay_device_get_num_relays(devicePtr)
 	}
-	
+
+    /**
+	 * Open device that serial number is serialNumber
+     * @param {string} serialNumber device serial number
+     * @returns {deviceHandler} returns a valid handle to the device on success or NULL on failure
+     */
 	api.openDeviceWithSerial = function(serialNumber){
 		return usbLib.usb_relay_device_open_with_serial_number(serialNumber, serialNumber.length);
 	}
-	
+
+    /**
+	 * Open a USB relay device
+     * @param {devicePtr} devicePtr pointer to the device
+     * @returns {deviceHandler} returns a valid handle to the device on success or NULL on failure.
+     */
 	api.openDevice = function(devicePtr){
 		return usbLib.usb_relay_device_open(devicePtr);
 	}
-	
+
+    /**
+	 * Close a USB relay device
+     * @param {deviceHandler} deviceHandler device handler which usb relay device your want to operate
+     * @returns TODO
+     */
 	api.closeDevice = function(deviceHandler){
-		return usbLib.usb_relay_device_close(deviceHandler);	
+		return usbLib.usb_relay_device_close(deviceHandler);
 	}
-	
+
+    /**
+	 * Turn ON a relay channel on the USB-Relay-Device
+     * @param {deviceHandler} deviceHandler device handler which usb relay device your want to operate
+     * @param {channelNumber} channelNumber the channel number: 1...max
+     * @returns {number} 0 -- success; 1 -- error; 2 -- index is invalid
+     */
 	api.openChannel = function(deviceHandler, channelNumber){
 		return usbLib.usb_relay_device_open_one_relay_channel(deviceHandler, channelNumber)
 	}
-	
+
+    /**
+     * Turn OFF a relay channel on the USB-Relay-Device
+     * @param {deviceHandler} deviceHandler device handler which usb relay device your want to operate
+	 * @param {number} channelNumber the channel number: 1...max
+     * @returns {number} 0 -- success; 1 -- error
+     */
 	api.closeChannel = function(deviceHandler, channelNumber){
 		return usbLib.usb_relay_device_close_one_relay_channel(deviceHandler, channelNumber)
 	}
-	
+
+    /**
+	 * Turn ON all relay channels on the USB-Relay-Device
+     * @param {deviceHandler} deviceHandler device handler which usb relay device your want to operate
+     * @returns {number} 0 -- success; 1 -- error
+     */
 	api.openAllChannels = function(deviceHandler){
 		return usbLib.usb_relay_device_open_all_relay_channel(deviceHandler);
 	}
 
+    /**
+     * Turn OFF all relay channels on the USB-Relay-Device
+     * @param {deviceHandler} deviceHandler device handler which usb relay device your want to operate
+     * @returns {number} 0 -- success; 1 -- error
+     */
 	api.closeAllChannels = function(deviceHandler){
 		return usbLib.usb_relay_device_close_all_relay_channel(deviceHandler);
 	}
-	
+
+    /**
+	 * Get state of all channels of the  USB-Relay-Device
+     * @param {deviceHandler}
+     * @returns TODO
+     */
 	api.getStatus = function(deviceHandler){
 		return usbLib.usb_relay_device_get_status_bitmap(deviceHandler);
 	}
